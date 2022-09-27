42260aa3673420f46d57da83fdf52b3dcccc136b
Nasrin <a.navabi65@gmail.com>
2022-09-27 17:06:30 +0200

    add translation

--- products/views.py
+++ products/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
+from django.http import HttpResponse
+from django.utils.translation import gettext as _
 
-from .models import Product
+from .models import Product, Comment
+from .forms import CommentForm
 
 
 class ProductListView(generic.ListView):
@@ -15,3 +18,34 @@ class ProductDetailView(generic.DetailView):
     model = Product
     template_name = 'products/product_detail.html'
     context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()
+        return context
+
+
+def test_translation(request):
+    result = _('hello')
+    return HttpResponse(result)
+
+
+class CommentCreateView(generic.CreateView):
+    model = Comment
+    form_class = CommentForm
+
+    # def get_success_url(self):
+    #     return reverse('product_list')
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+
+        product_id = int(self.kwargs['product_id'])
+        products = get_object_or_404(Product, id=product_id)
+        obj.products = products
+
+        return super().form_valid(form)
+
+
+
